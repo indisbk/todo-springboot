5d443714104fc7ba706c304c9de2d027487811c1
Tsyrempilov Victor <indisbk@gmail.com>
2021-01-10 20:37:11 +0300

    0.0.1 Initial commit(add connection DB)

---
+++ src/test/java/com/learnindi/todo/todospringboot/TodoSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learnindi.todo.todospringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
