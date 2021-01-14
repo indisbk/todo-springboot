ea038be881c4d99c1c265e5c147002167c5c1646
Tsyrempilov Victor <indisbk@gmail.com>
2021-01-14 20:34:46 +0300

    0.0.4 Realized add and update methods in priority/category controllers

--- src/main/java/com/learnindi/todo/todospringboot/controller/CategoryController.java
+++ src/main/java/com/learnindi/todo/todospringboot/controller/CategoryController.java
@@ -3,9 +3,9 @@ package com.learnindi.todo.todospringboot.controller;
 import com.learnindi.todo.todospringboot.entity.Category;
 import com.learnindi.todo.todospringboot.repo.CategoryRepository;
 import org.springframework.data.domain.Sort;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -26,5 +26,31 @@ public class CategoryController {
     @GetMapping("/test")
     public List<Category> test() {
         return categoryRepository.findAll(Sort.by(Sort.Order.asc("id")));
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<Category> addPriority(@RequestBody Category category) {
+        if (category.getId() != null && category.getId() != 0) {
+            return new ResponseEntity("Redundant parameter: id must be null!", HttpStatus.NOT_ACCEPTABLE);
+        }
+
+        if (category.getTitle() == null || category.getTitle().trim().length() == 0) {
+            return new ResponseEntity("Missed parameter: title", HttpStatus.NOT_ACCEPTABLE);
+        }
+
+        return ResponseEntity.ok(categoryRepository.save(category));
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<Category> updatePriority(@RequestBody Category category) {
+        if (category.getId() == null || category.getId() == 0) {
+            return new ResponseEntity("Missed parameter: id can't be null or zero!", HttpStatus.NOT_ACCEPTABLE);
+        }
+
+        if (category.getTitle() == null || category.getTitle().trim().length() == 0) {
+            return new ResponseEntity("Missed parameter: title", HttpStatus.NOT_ACCEPTABLE);
+        }
+
+        return ResponseEntity.ok(categoryRepository.save(category));
     }
 }
--- src/main/java/com/learnindi/todo/todospringboot/controller/PriorityController.java
+++ src/main/java/com/learnindi/todo/todospringboot/controller/PriorityController.java
@@ -2,9 +2,9 @@ package com.learnindi.todo.todospringboot.controller;
 
 import com.learnindi.todo.todospringboot.entity.Priority;
 import com.learnindi.todo.todospringboot.repo.PriorityRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,5 +25,39 @@ public class PriorityController {
     @GetMapping("/test")
     public List<Priority> test() {
         return priorityRepository.findAll();
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<Priority> addPriority(@RequestBody Priority priority) {
+        if (priority.getId() != null && priority.getId() != 0) {
+            return new ResponseEntity("Redundant parameter: id must be null!", HttpStatus.NOT_ACCEPTABLE);
+        }
+
+        if (priority.getTitle() == null || priority.getTitle().trim().length() == 0) {
+            return new ResponseEntity("Missed parameter: title", HttpStatus.NOT_ACCEPTABLE);
+        }
+
+        if (priority.getColor() == null || priority.getColor().trim().length() == 0) {
+            return new ResponseEntity("Missed parameter: color", HttpStatus.NOT_ACCEPTABLE);
+        }
+
+        return ResponseEntity.ok(priorityRepository.save(priority));
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<Priority> updatePriority(@RequestBody Priority priority) {
+        if (priority.getId() == null || priority.getId() == 0) {
+            return new ResponseEntity("Missed parameter: id can't be null or zero!", HttpStatus.NOT_ACCEPTABLE);
+        }
+
+        if (priority.getTitle() == null || priority.getTitle().trim().length() == 0) {
+            return new ResponseEntity("Missed parameter: title", HttpStatus.NOT_ACCEPTABLE);
+        }
+
+        if (priority.getColor() == null || priority.getColor().trim().length() == 0) {
+            return new ResponseEntity("Missed parameter: color", HttpStatus.NOT_ACCEPTABLE);
+        }
+
+        return ResponseEntity.ok(priorityRepository.save(priority));
     }
 }
